482e4982f6502f521d6c8aa0526f8ec71b1307e0
Alex Leiba <alexandru@enovatika.com>
2025-01-03 14:22:19 +0200

    sdds

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,32 +30,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <html lang='en'>
       <Head>
         <meta
           name='facebook-domain-verification'
           content='s74sqo1gwaar8ywpumci3rx1vray8d'
         />
       </Head>
-      <html lang='en'>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <AuthSessionProvider>
-            <div className='flex min-h-[100vh] bg-white dark:bg-black flex-col'>
-              <header>
-                <Header />
-              </header>
-              <Spacer size={14} /> {/*Header size*/}
-              <main className='flex-grow'>{children}</main>
-              <footer>
-                <Footer />
-              </footer>
-            </div>
-            <Toaster />
-          </AuthSessionProvider>
-        </body>
-      </html>
-    </>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <AuthSessionProvider>
+          <div className='flex min-h-[100vh] bg-white dark:bg-black flex-col'>
+            <header>
+              <Header />
+            </header>
+            <Spacer size={14} /> {/*Header size*/}
+            <main className='flex-grow'>{children}</main>
+            <footer>
+              <Footer />
+            </footer>
+          </div>
+          <Toaster />
+        </AuthSessionProvider>
+      </body>
+    </html>
   );
 }
