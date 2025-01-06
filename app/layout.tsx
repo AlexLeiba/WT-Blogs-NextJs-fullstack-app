f32a496ebe5dc410654b64a881c3bb0b723c4528
Alex Leiba <alexandru@enovatika.com>
2025-01-06 16:39:03 +0200

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthSessionProvider>
-          <div className='flex min-h-[100vh] bg-white dark:bg-black flex-col'>
+          <div className='flex min-h-[100vh] bg-white dark:bg-black flex-col text-black dark:text-white '>
             <header>
               <Header />
             </header>
